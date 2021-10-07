aa5a879276eb6f2c11eff45a8a007216b3d2e531
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-10-07 19:12:43 +0200

    organiz front

--- javascript.js
+++ javascript.js
@@ -1,16 +1,29 @@
 console.log(Vue)
 
 const app=new Vue({
-
 el:'#landing',
 data:{
-toggle:false
+toggle:false,
+toggleInscritpion:false,
+pass1:"",
+pass2:"",
 },
 methods:{
 changeToggle(){
     this.toggle=!this.toggle
     console.log(this.toggle)
     return this.toggle
+},
+changeToggleInscription(){
+    this.toggleInscritpion=!this.toggleInscritpion
+    console.log(this.toggleInscritpion)
+    return this.toggleInscritpion
+},
+preventDefault(event){
+   console.log(this.pass1)
+   console.log(this.pass2)
+    event.preventDefault()
+
 }
 }
 })
